915a3a5030c0ca53fd36e26fb1422180680d3d6c
Royall Spence <royall@royall.us>
2018-01-28 16:28:51 -0500

    Set enabled radio on pageload

--- domains.js
+++ domains.js
@@ -1,8 +1,32 @@
-function setPageState(e) {
-    e.preventDefault();
-    browser.storage.sync.get({
-        color: document.querySelector("#color").value
-    });
+function initPageState(e) {
+    var settings = browser.storage.sync.get("domain-intercept");
+
+    if(typeof settings.enabled !== "undefined" && settings.enabled === true){
+        document.getElementById("interceptOn").setAttribute("checked", true);
+        document.getElementById("interceptOff").removeAttribute("checked");
+    }else{
+        document.getElementById("interceptOn").removeAttribute("checked");
+        document.getElementById("interceptOff").setAttribute("checked", true);
+    }
+
+    // TODO generate record pairs and write them to the page
 }
 
-// window.onload = setPageState;
+function saveEnabled(enabled) {
+    alert("saving enabled state as " + enabled)
+    var settings = browser.storage.sync.get("domain-intercept");
+    settings.enabled = enabled
+    browser.storage.sync.set({
+
+    })
+}
+
+function clickEnable() {
+    saveEnabled(true);
+}
+
+function clickDisable() {
+    saveEnabled(false);
+}
+
+window.onload = initPageState;
