7af60910daf12c8606df6305cc9d9dd401f1614f
Royall Spence <royall@royall.us>
2018-01-28 21:21:05 -0500

    Add records to storage

--- domains.js
+++ domains.js
@@ -1,38 +1,61 @@
-const onButton = document.getElementById("interceptOn"),
-    offButton = document.getElementById("interceptOff");
+"use strict";
+
+const addButton = document.getElementById("add"),
+    onButton    = document.getElementById("interceptOn"),
+    offButton   = document.getElementById("interceptOff");
+
+window.onload       = initPageState;
+onButton.onclick    = (function(){setEnabled(true)});
+offButton.onclick   = (function(){setEnabled(false)});
+addButton.onclick   = clickAdd;
 
 function initPageState(e) {
-    const settings = browser.storage.local.get("domain-intercept");
+    browser.storage.local.get().then(function (settings){
+        console.log(settings);
 
-    if(typeof settings.enabled !== "undefined" && settings.enabled === true){
-        onButton.setAttribute("checked", true);
-        offButton.removeAttribute("checked");
-    }else{
-        onButton.removeAttribute("checked");
-        offButton.setAttribute("checked", true);
-    }
+        if(settings.hasOwnProperty("enabled") && settings.enabled === true){
+            onButton.checked = true;
+        }else{
+            offButton.checked = true;
+        }
 
-    // TODO generate record pairs and write them to the page
+        document.querySelector("tbody").innerHTML = domainRows(settings.hasOwnProperty("records") ? settings.records : {});
+    });
 }
 
-function saveEnabled(enabled) {
-    var settings = browser.storage.local.get("domain-intercept");
-    settings.enabled = enabled;
-    browser.storage.local.set(settings);
+function domainRows(records) {
+    var out = [];
+
+    Object.entries(records).forEach(el => {
+        out.push(domainRow(el[0], el[1]));
+    });
+
+    return out.join("\n");
+}
+
+function domainRow(to, from) {
+    return `<tr><td>${to}</td><td>${from}</td><td><a href="#" class="remove">delete</a></td></tr>`;
+}
+
+function clickAdd() {
+    var from = document.getElementById("from").value,
+        to = document.getElementById("to").value;
+
+    browser.storage.local.get("records").then(function (records){
+        records = records.records || {};
+        records[from] = to;
+        browser.storage.local.set({"records": records}).then(function (){
+            document.getElementById("from").value = "";
+            document.getElementById("to").value = "";
+            initPageState(); // this handles updates and inserts equally well without extra code
+        });
+    });
 }
 
 function clickDelete(el) {
     // TODO delete this row and remove its entry from storage
 }
 
-function clickEnable() {
-    saveEnabled(true);
+function setEnabled(val) {
+    browser.storage.local.set({enabled: val});
 }
-
-function clickDisable() {
-    saveEnabled(false);
-}
-
-window.onload = initPageState;
-onButton.onclick = clickEnable;
-offButton.onclick = clickDisable;
